1c3b3476386dbb8533f71b4911d7a242cd4bfe21
bottle-catfish <armandurrani02@gmail.com>
2023-10-16 19:20:20 -0700

    Need error handling

--- pipe.c
+++ pipe.c
@@ -3,82 +3,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
+	//TO DO : ERROR HANDLING IN GENERAL 
+	//TO DO : ERROR HANDLING FOR TOO FEW ARGUMENTS
+	if (argc<2) {
+		return EINVAL;
+	}
 	if (argc==2) {
 		printf("Only one argument");
 		execlp(argv[1], argv[1], NULL);	
 	}
 	else if (argc>2) {
-		int o_stdin = dup(0);
-		int o_stdout = dup(1);
-		int piper [2];
-		pipe(piper);
 		for (int i = 1; i < argc; i++) {
-			if (i==1) {
-				int pid = fork();
-				if (pid==0) {
-					close(piper[0]);
-					close(piper[1]);
-					execlp(argv[i], argv[i], NULL);
-				}
-				else {
-					close(piper[0]);
+			int piper[2];
+			pipe(piper);
+			int pid = fork();
+			if (pid==0) {
+				if (i!=argc-1) {
 					close(1);
 					dup(piper[1]);
 					close(piper[1]);
-					int status = 0;
-					waitpid(pid, &status, 0);
 				}
-			}
-			else if (i==argc-1) {
-				int pid = fork();
-				printf("Last Argument\n");
-				if (pid==0) {
-					close(piper[1]);
-					close(0);
-					dup(piper[0]);
-					close(piper[0]);
-					execlp(argv[i], argv[i], NULL);
+				if (execlp(argv[i], argv[i], NULL)==-1) {
+					exit(errno);
 				}
-				else {
-					dup2(o_stdout, 1);
-					close(piper[0]);
-					close(piper[1]);
-					int status = 0;
-					waitpid(pid, &status, 0);
-				}
+				
 			}
 			else {
-				int pid = fork();
-				printf("And beyond!\n");
-				if (pid==0) {
-					close(piper[1]);
-					close(0);
-					dup(piper[0]);
-					close(piper[0]);
-					execlp(argv[i], argv[i], NULL);
+				int status = 0;
+				waitpid(pid, &status, 0);
+				if (WIFEXITED(status)==-1) {
+					return WEXITSTATUS(status);
 				}
-				else {
-					close(piper[0]);
-					close(1);
-					dup(piper[1]);
-					close(piper[1]);
-					int status = 0;
-					waitpid(pid, &status, 0);
-				}
-
+				// check the status here to do error handling chief
+				close(piper[1]);
+				close(0);
+				dup(piper[0]);
+				close(piper[0]);
 			}
 		}
-		
-		//parent output reset?
-		//child reading must be reset every single time
 	}
-
-
-
-
-
 	return 0;
 }
